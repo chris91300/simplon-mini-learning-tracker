d6e6dae369293d8de3cdd20435b47f2b0bfa6740
MICHEL Christophe <christophe.michel@externe.maif.fr>
2026-06-30 09:54:50 +0200

    setup frontend and backend

---
+++ backend/src/main/java/com/simplon/miniLearningTracker/MiniLearningTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.miniLearningTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLearningTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLearningTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/simplon/miniLearningTracker/MiniLearningTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.miniLearningTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLearningTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
